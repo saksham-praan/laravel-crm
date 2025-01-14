4fff14cb00d05cdf5da2cd63dd6fffabddd9e198
saksham-praan <saksham@praan.io>
2025-01-14 16:09:45 +0530

    Revert "http changed to https"
    This reverts commit e3278f1bc22d4443e9fb7a3a79b80426a185d14d.

--- packages/Webkul/WebForm/src/Resources/views/components/spinner/index.blade.php
+++ packages/Webkul/WebForm/src/Resources/views/components/spinner/index.blade.php
@@ -2,7 +2,7 @@
 @props(['color' => 'currentColor'])
 
 <svg
-    xmlns="https://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
     fill="none"
     aria-hidden="true"
     viewBox="0 0 24 24"
